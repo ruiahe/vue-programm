308863aef2bf9cc7c615c2fd048caf8cbeaad66c
盒子 <729941315@qq.com>
2019-08-12 01:07:24 +0800

    emotion

--- src/page/detail/detail.vue
+++ src/page/detail/detail.vue
@@ -78,11 +78,20 @@
                     </div>
                 </div>
                 <div class="comment-txt container">
-                    <textarea name="" placeholder="评论点什么..." type='search' @input='change_num()' v-model='comment'></textarea>
+                    <textarea name="" placeholder="评论点什么..." type='search' @input='change_num()' v-model='commentText'></textarea>
+                    <div class="textarea" contenteditable="trrue" @focus="del()" @input="change_dev()">{{commentT}}</div>
                 </div>
                 <div class="comment-emoji container space-between">
                     <span class='emoji'></span>
                     <span class='emoji-txt'>{{commentLen}}/200</span>
+                </div>
+                <div class="emotion" >
+                    <textarea class="text" rows="5" v-model="content" ></textarea>
+                    <emotion @emotion="handleEmotion" :height="200" ></emotion>
+                    <div class="text-place">
+                    <!-- /\#[\u4E00-\u9FA5]{1,3}\;/gi 匹配出含 #XXX; 的字段 -->
+                    <p v-html="content.replace(/\#[\u4E00-\u9FA5]{1,3}\;/gi, emotion)"></p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -107,10 +116,12 @@
     import {Request} from '@/common/js/api.js'
     import wHead from '../windowHead/windowHead'
     import { common } from '../../common/js/common';
+    import Emotion from '@/page/emotion/index'
     export default {
         name: 'detail',
         components:{
-            wHead
+            wHead,
+            Emotion
         },
         data() {
             return {
@@ -122,9 +133,12 @@
                     hasRed: false,
                 },
                 makeStatement: {},
-                comment: '',                                        //评论内容
+                commentText: '',                                        //评论内容
                 commentLen: 0,
-                operaId: {}
+                operaId: {},
+                content: '',
+                comment: '',
+                commentT: '评论点什么...'
             }
         },
         mounted () {
@@ -132,6 +146,14 @@
             this.getDate();
         },
         methods:{
+            del(){
+                if(this.commentT == '评论点什么...'){
+                    this.commentT = ' ';
+                }
+            },
+            change_dev(){
+                
+            },
             interactive(){
                 alert('要跳转拉');
                 var json = {
@@ -151,11 +173,11 @@
                 var _this = this;
                 new Request('app/forum/userReplyForumInfo',{
                     "titleId": this.operaId,
-                    "replyContent":this.comment,
+                    "replyContent":_this.commentText,
                     "isOne":false,
                 } , 'post' ,'ios' ,'2.0.0', (data) => {
                     _this.close_comment();
-                    _this.comment = '';
+                    _this.commentText = '';
                     _this.getDate();
                 }, (err) => {
                     console.log('这里是错误回调');
@@ -168,8 +190,8 @@
                 if(num < 200) {
                     this.commentLen = num++;
                 } else {
-                    this.comment = this.comment.substr(0,200);
-                    this.commentLen = this.comment.length;
+                    this.commentText = this.commentText.substr(0,200);
+                    this.commentLen = this.commentText.length;
                 }
             },
             // 打开发言框
@@ -189,11 +211,21 @@
                     console.log('这里是错误回调');
                     console.log(err);
                 });
+            },
+            handleEmotion (i) {
+                this.content += i
+            },
+            // 将匹配结果替换表情图片
+            emotion (res) {
+                let word = res.replace(/\#|\;/gi,'')
+                const list = ['微笑', '撇嘴', '色', '发呆', '得意', '流泪', '害羞', '闭嘴', '睡', '大哭', '尴尬', '发怒', '调皮', '呲牙', '惊讶', '难过', '酷', '冷汗', '抓狂', '吐', '偷笑', '可爱', '白眼', '傲慢', '饥饿', '困', '惊恐', '流汗', '憨笑', '大兵', '奋斗', '咒骂', '疑问', '嘘', '晕', '折磨', '衰', '骷髅', '敲打', '再见', '擦汗', '抠鼻', '鼓掌', '糗大了', '坏笑', '左哼哼', '右哼哼', '哈欠', '鄙视', '委屈', '快哭了', '阴险', '亲亲', '吓', '可怜', '菜刀', '西瓜', '啤酒', '篮球', '乒乓', '咖啡', '饭', '猪头', '玫瑰', '凋谢', '示爱', '爱心', '心碎', '蛋糕', '闪电', '炸弹', '刀', '足球', '瓢虫', '便便', '月亮', '太阳', '礼物', '拥抱', '强', '弱', '握手', '胜利', '抱拳', '勾引', '拳头', '差劲', '爱你', 'NO', 'OK', '爱情', '飞吻', '跳跳', '发抖', '怄火', '转圈', '磕头', '回头', '跳绳', '挥手', '激动', '街舞', '献吻', '左太极', '右太极']
+                let index = list.indexOf(word)
+                return `<img src="https://res.wx.qq.com/mpres/htmledition/images/icon/emotion/${index}.gif" align="middle">`   
             }
         },
         beforeCreate(){
             document.querySelector('body').style='background:#fff;';
-        },
+        }
     }
 </script>
 <style lang="less" scoped>
