4b76a7016e75b8759105e2c75ba98f56b873c69f
盒子 <729941315@qq.com>
2019-11-28 08:35:15 +0800

    修改前一天晚上提交的错误文件

--- src/App.vue
+++ src/App.vue
@@ -20,29 +20,29 @@ export default {
   beforeCreate(){
     const _this = this;
     // 本地调试时手动调用设置token的方法
-    // set_headers();
+    set_headers();
     
     // app交互 -- ios传输app token等信息调用的方法
-    window['getJson'] = (json) => {
-      _this.getAppInfo(json);
-      set_headers();
-    }
+    // window['getJson'] = (json) => {
+    //   _this.getAppInfo(json);
+    //   set_headers();
+    // }
     // app交互 -- 安卓调用返回上一页方法
-    window['getback'] = () => {
-      const path = this.$route.path;
-      const bol = path.indexOf('feed') > -1 || path.indexOf('mortgageCaculate/caculate') > -1 || path.indexOf('personalIncomeTax/input') > -1 || path.indexOf('todayInHistory/list') > -1 || path.indexOf('timeSelector/index') > -1;
-      common.back(bol)
-    }
+    // window['getback'] = () => {
+    //   const path = this.$route.path;
+    //   const bol = path.indexOf('feed') > -1 || path.indexOf('mortgageCaculate/caculate') > -1 || path.indexOf('personalIncomeTax/input') > -1 || path.indexOf('todayInHistory/list') > -1 || path.indexOf('timeSelector/index') > -1;
+    //   common.back(bol)
+    // }
     // app交互 -- 分别根据安卓 & ios调用app方法
-    if(common.isAndroid()){
-      let info = window.Android.getInfo();
-      setTimeout(()=>{
-        _this.getAppInfo(JSON.parse(info));
-      })
-      set_headers();
-    } else if(common.isIos()){
-      window.webkit.messageHandlers.linkTo.postMessage({'methodName': 'getInfo'});
-    }
+    // if(common.isAndroid()){
+    //   let info = window.Android.getInfo();
+    //   setTimeout(()=>{
+    //     _this.getAppInfo(JSON.parse(info));
+    //   })
+    //   set_headers();
+    // } else if(common.isIos()){
+    //   window.webkit.messageHandlers.linkTo.postMessage({'methodName': 'getInfo'});
+    // }
   }
 }
 </script>
